6c4dc958ad886e7365e2eb21ad0ba89ccb49c459
vkluchysvet <svetlanatylerjoshya@gmail.com>
2021-12-07 22:11:54 +0300

    Добавьте файлы проекта.

---
+++ AppLifecycleTutorial/AppLifecycleTutorial/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppLifecycleTutorial
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            entry.Text = (Application.Current as App).DisplayText;
+        }
+
+        void OnEntryCompleted(object sender, EventArgs e)
+        {
+            (Application.Current as App).DisplayText = entry.Text;
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
